097e228e6ae5653708f546c06c477830d4625a51
ShurGith <75266693+ShurGith@users.noreply.github.com>
2025-04-25 17:39:12 +0200

    formulario contacto

--- Proyecto/src/pages/Contacto.jsx
+++ Proyecto/src/pages/Contacto.jsx
@@ -1,7 +1,25 @@
 
 function Contacto() {
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log('Formulario enviado')
+    }
     return (
-        <h1>Contacto</h1>
+        <>
+            <h1 className="title-contacto">Contacto</h1>
+            <form className="form-contacto" onSubmit={handleSubmit}>
+                <input className="input-contacto"
+                    type="text"
+                    placeholder="Ingresa tu Nombre"
+
+                />
+                <input className="input-contacto"
+                    type="email"
+                    placeholder="Ingresa tu Email"
+                />
+                <button className="btn-contacto" type="submit">Enviar</button>
+            </form>
+        </>
     )
 }
 
